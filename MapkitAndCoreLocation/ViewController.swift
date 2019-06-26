2ae678b188fb388168cb73306eea8e192c67f8dd
Annisa Nabila Nasution <annisa.nasution@binus.ac.id>
2019-06-26 17:17:49 +0700

    Initial Commit

---
+++ MapkitAndCoreLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapkitAndCoreLocation
+//
+//  Created by Annisa Nabila Nasution on 26/06/19.
+//  Copyright © 2019 Annisa Nabila Nasution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
